ce972474dbe288a45e6e567cff21d928ee2f9e0d
joseph sortino <josephsortino@hotmail.com>
2020-03-03 02:10:45 -0500

    completed

--- url_parser.rb
+++ url_parser.rb
@@ -1,30 +1,73 @@
+require 'uri'
+require 'cgi'
+
 class UrlParser
+
 def initialize(url)
 @url = url
 puts @url
 end
 
-
 def scheme
-    # 'http://www.google.com:60/search?q=cat&name=Tim#img=FunnyCat'.split(":").first
-    # 'http://www.google.com:60/search?q=cat&name=Tim#img=FunnyCat'.split(":").last.split("/").first
-    
+        
     @scheme = @url.split(":").first
 end
 
 def port
 
-    @scheme = @url.split(":").first
-    @extractPort  = @url.split(":").last.split("/").first
+    scheme = @url.split(":").first
+    extractPort  = @url.split(":").last.split("/").first
 
-    if @scheme == "http" && @extractPort == ''
-      @port = "80"
+    if scheme == "http" && extractPort == ''
+      port = "80"
 
-    elsif @scheme == "https" &&  @extractPort == ''
-      @port = "443"
-    else @extractPort
+    elsif scheme == "https" &&  extractPort == ''
+      port = "443"
+    else extractPort
     end
 end
 
+def domain
+  @uri = URI.parse(@url)
+  @uri.host
+
+end
+
+def fragment_id
+  @url.split("#").last
+end
+
+def path
+path = @url.split("?").first.split("//").last.split("/") 
+
+if path.length > 1
+  path.last
+  else nil
+  end
+
+end
+
+def query_string
+
+  query = @url.split("?").last.split("#").first
+
+  hashOne = query.split("&").first
+  keyOne = "#{hashOne.split("=").first}"
+  first_value = "#{hashOne.split("=").last}"
+
+  hashTwo = query.split("&").last
+  keyTwo = "#{hashTwo.split("=").first}"
+  second_value = "#{hashTwo.split("=").last}"
+
+  if hashTwo == ""
+    @query_string = {keyOne => first_value}
+  elsif keyOne == keyTwo
+    @query_string = {keyTwo => second_value}
+  else
+    @query_string = {keyOne => first_value, keyTwo => second_value}
+  end
+return @query_string
+
+end
 
 end
